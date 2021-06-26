d2ff5c4573d3b63d15c1c48e32166d73523f5237
James Lucktaylor <jlucktay+github@gmail.com>
2021-06-26 15:44:07 +0100

    test: add a test to look for strings in response bodies

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.15
 require (
 	github.com/go-chi/chi/v5 v5.0.3
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/matryer/is v1.4.0
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.8.0
 	github.com/yosssi/gohtml v0.0.0-20201013000340-ee4748c638f4
---
+++ server_test.go
@@ -0,0 +1,79 @@
+package main_test
+
+import (
+	"context"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/matryer/is"
+	"google.golang.org/api/idtoken"
+
+	main "go.jlucktay.dev/servers.menagerie.games"
+)
+
+func TestBodyContains(t *testing.T) {
+	is := is.New(t)
+
+	s := main.Server{
+		Config: main.Config{
+			AuthorisedSubjects: []string{"one", "two", "three"},
+			ClientID:           "client_id",
+		},
+		Router: chi.NewRouter(),
+		TokenVerifier: func(idToken, audience string) (*idtoken.Payload, error) {
+			return &idtoken.Payload{
+				Claims: map[string]interface{}{
+					"foo":   "bar",
+					"baz":   "qux",
+					"quux":  "garply",
+					"waldo": "fred",
+					"plugh": "xyzzy",
+				},
+
+				Audience: "audience",
+				Issuer:   "issuer",
+				Subject:  "subject",
+
+				// 50 year token, from 2000 to 2050
+				IssuedAt: 946684800,
+				Expires:  4102444800,
+			}, nil
+		},
+	}
+
+	s.Initialise()
+
+	ts := httptest.NewServer(s.Router)
+	t.Cleanup(ts.Close)
+
+	testCases := map[string]struct {
+		method            string
+		path              string
+		bodyShouldContain string
+	}{
+		"Look for the client ID in the root page": {
+			method:            http.MethodGet,
+			path:              "/",
+			bodyShouldContain: `<meta name="google-signin-client_id" content="client_id.apps.googleusercontent.com" />`,
+		},
+	}
+	for name, tC := range testCases {
+		t.Run(name, func(t *testing.T) {
+			req, err := http.NewRequestWithContext(context.Background(), tC.method, ts.URL+"/", nil)
+			is.NoErr(err)
+
+			resp, err := http.DefaultClient.Do(req)
+			is.NoErr(err)
+
+			respBody, err := ioutil.ReadAll(resp.Body)
+			is.NoErr(err)
+			t.Cleanup(func() { is.NoErr(resp.Body.Close()) })
+
+			is.True(strings.Contains(string(respBody), tC.bodyShouldContain))
+		})
+	}
+}
