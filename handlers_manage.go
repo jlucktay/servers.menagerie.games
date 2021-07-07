d3ee6c54629a79716fe7aea8382713a979740ff6
James Lucktaylor <jlucktay+github@gmail.com>
2021-07-07 09:33:32 +0100

    feat(manage): progress on instance creation

--- handlers_manage.go
+++ handlers_manage.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,6 +12,10 @@ import (
 	"github.com/spf13/viper"
 	"google.golang.org/api/compute/v1"
 )
+
+var ErrNoInstTemplates = errors.New("no instance templates found")
+
+var locations []location
 
 // location describes the structure of a JSON file that we use to denote which GCP regions and zones are in use by this
 // project.
@@ -31,7 +38,8 @@ func (s *Server) manageGetHandler() http.HandlerFunc {
 
 	return func(w http.ResponseWriter, r *http.Request) {
 		init.Do(func() {
-			locations, err := getLocationsFromStorage(r.Context(), s.Config.Manage.Bucket, s.Config.Manage.Object)
+			var err error
+			locations, err = getLocationsFromStorage(r.Context(), s.Config.Manage.Bucket, s.Config.Manage.Object)
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				log.Print(err)
@@ -67,22 +75,84 @@ func (s *Server) managePostHandler(w http.ResponseWriter, r *http.Request) {
 	svc, err := compute.NewService(r.Context())
 	if err != nil {
 		log.Printf("could not create new Compute service: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 
 		return
 	}
 
-	tmplListCall := svc.InstanceTemplates.List(viper.GetString("CLOUDSDK_CORE_PROJECT"))
+	if errDel := deleteRunningInstances(r.Context(), svc); errDel != nil {
+		log.Printf("could not delete running instances: %v", errDel)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 
-	list, err := tmplListCall.Do()
+		return
+	}
+
+	template, err := getLatestTemplate(r.Context(), svc)
 	if err != nil {
-		log.Printf("could not list instance templates from Compute service: %v", err)
+		log.Printf("could not get latest template: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 
 		return
 	}
 
-	if _, err := w.Write([]byte(fmt.Sprintf("POST /manage\n%#v", list.Items))); err != nil {
+	buf := bytes.NewBufferString("POST /manage\n" + template)
+
+	if _, err := w.Write(buf.Bytes()); err != nil {
 		log.Printf("could not write bytes to response: %v", err)
 
 		return
 	}
+}
+
+func deleteRunningInstances(ctx context.Context, svc *compute.Service) error {
+	wg := sync.WaitGroup{}
+
+	for _, loc := range locations {
+		instListCall := svc.Instances.List(viper.GetString("CLOUDSDK_CORE_PROJECT"), loc.Zone)
+		instListCall.Context(ctx)
+
+		list, err := instListCall.Do()
+		if err != nil {
+			return fmt.Errorf("could not list instances from Compute service: %w", err)
+		}
+
+		for _, item := range list.Items {
+			wg.Add(1)
+
+			go func(name string) {
+				defer wg.Done()
+
+				deleteCall := svc.Instances.Delete(viper.GetString("CLOUDSDK_CORE_PROJECT"), loc.Zone, name)
+				deleteCall.Context(ctx)
+
+				if _, err := deleteCall.Do(); err != nil {
+					log.Printf("could not delete instance from Compute service: %v", err)
+
+					return
+				}
+			}(item.Name)
+		}
+	}
+
+	wg.Wait()
+
+	return nil
+}
+
+func getLatestTemplate(ctx context.Context, svc *compute.Service) (string, error) {
+	tmplListCall := svc.InstanceTemplates.List(viper.GetString("CLOUDSDK_CORE_PROJECT"))
+	tmplListCall.Context(ctx)
+	tmplListCall.MaxResults(1)
+	tmplListCall.OrderBy("creationTimestamp desc")
+
+	list, err := tmplListCall.Do()
+	if err != nil {
+		return "", fmt.Errorf("could not list instance templates from Compute service: %w", err)
+	}
+
+	if len(list.Items) < 1 {
+		return "", ErrNoInstTemplates
+	}
+
+	return list.Items[0].Name, nil
 }
